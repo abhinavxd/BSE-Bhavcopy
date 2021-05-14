663bc38a9ba6d7ad5dd4b1cac3ff05f40850e272
Abhinav Raut <abhinavrautcs@gmail.com>
2021-05-14 05:51:21 +0530

    remove unsed code from homepage component

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import axios from "axios";
 import swal from 'sweetalert';
@@ -8,7 +8,6 @@ import "react-awesome-button/dist/styles.css";
 import { CSVLink } from "react-csv";
 import DataTable from 'react-data-table-component';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
 
 const Homepage = () => {
     const [searchTerm, setSearchTerm] = useState('');
