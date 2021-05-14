7ee244d6bc62e21f803932a4715ac09fa376bc40
Abhinav Raut <abhinavrautcs@gmail.com>
2021-05-14 18:49:32 +0530

    remove main.js from gitignore and add localhost:5000 to allowed hosts

--- bse_bhavcopy/settings.py
+++ bse_bhavcopy/settings.py
@@ -3,7 +3,7 @@ import environ
 import os
 
 env = environ.Env()
-environ.Env.read_env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +18,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'bse-equity-bhavcopy.herokuapp.com']
+ALLOWED_HOSTS = ['localhost:5000', 'bse-equity-bhavcopy.herokuapp.com']
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
