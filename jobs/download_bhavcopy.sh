79a010baca6414c3eed57a1adaed44d5c464f174
Abhinav Raut <abhinavrautcs@gmail.com>
2021-05-15 04:18:14 +0530

    bash script to run Django command everyday at 18:00 IST

---
+++ jobs/download_bhavcopy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sleep 1s
+
+#For first time execution set last run date less than todays date.
+lastRunDate="13-05-2021"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APPDIR=${SCRIPTDIR}/..
+
+
+while true
+do
+	LOG_FILE=${APPDIR}/logs/job-log-`date +%Y-%m-%d`.log
+
+    # Get current hour, day, month and year for Asia/Calcutta region
+    H=$(TZ=":Asia/Calcutta" date +%H)
+    D=$(TZ=":Asia/Calcutta" date +%d)
+    M=$(TZ=":Asia/Calcutta" date +%m)
+    Y=$(TZ=":Asia/Calcutta" date +%y)
+
+    # Get todays date for Asia/Calcutta region
+    todaysDate=$(TZ=":Asia/Calcutta" date +%d-%m-%Y)
+
+    echo "Current time ${H} hours"  >> ${LOG_FILE} 2>&1
+    echo "Last run date is ${lastRunDate}"  >> ${LOG_FILE} 2>&1
+    echo "Todays date is ${todaysDate}"  >> ${LOG_FILE} 2>&1
+
+    # Run everyday at 18:00 IST
+    if [[ ${lastRunDate} != ${todaysDate} ]] && ((10#${H} >= 18 )); then
+        # Sleep for a 5 minutes, just to make sure that this job doesn't miss the uploaded file.
+        sleep 5m
+        printf "Job starting. \n" >> ${LOG_FILE} 2>&1
+        python ${APPDIR}/manage.py importBhavCopy ${D}-${M}-${Y}
+        lastRunDate=${todaysDate}
+        printf "\n\nChanged lastRunDate to ${D}-${M}-${Y}\n" >> ${LOG_FILE} 2>&1
+    fi
+    echo "-----------------------------------------------------------------------------------"  >> ${LOG_FILE} 2>&1
+    sleep 2m
+done
+
+
